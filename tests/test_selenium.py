50bd36921dc258a26e2f2babe225ae56ed4cb207
John Harrison <balanceofprobability@gmail.com>
2019-07-23 02:47:05 -0700

    run integration test in pyvirtualdisplay
    Changes in travis:
     - add geckodriver in before_install
     - add firefox to before_install
     - add scrot to before_install
     - set export DISPLAY=:0 in before_script
    
    Add test_selenium.py:
     - setup/teardown w/ pyvirtualdisplay
     - setup/teardown w/ selenium webdriver
     - monkey-patch pyautogui x11 internals w/ Xlib display
    
    Changes to requirements.txt:
     - add pyvirtualdisplay & Xlib

---
+++ tests/test_selenium.py
@@ -0,0 +1,53 @@
+from decaptcha import *
+import pytest
+import pyautogui
+from pyvirtualdisplay import Display
+from selenium import webdriver
+import time
+import os
+import Xlib.display
+
+
+@pytest.fixture(scope="session")
+def vdisplay():
+    return Display(visible=0, size=(1080, 720))
+
+
+def test_vdisplay_setup(vdisplay):
+    vdisplay.start()
+
+
+@pytest.fixture(scope="session")
+def browser():
+    return webdriver.Firefox()
+
+
+def test_browser_setup(browser):
+    browser.maximize_window()
+    browser.get("https://www.google.com/recaptcha/api2/demo")
+
+
+def test_title(browser):
+    assert "ReCAPTCHA demo" in browser.title
+
+
+def test_x11_display():
+    pyautogui._pyautogui_x11._display = Xlib.display.Display(os.environ["DISPLAY"])
+
+
+@pytest.fixture
+def bot():
+    return NotARobot()
+
+
+def test_bot(bot):
+    bot.run()
+
+
+def test_teardown(browser):
+    browser.close()
+    browser.quit()
+
+
+def test_xvfb_teardown(vdisplay):
+    vdisplay.stop()
