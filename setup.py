cce43fc8c03b27ecd4e74b82233fb6e15c4979ad
John Harrison <balanceofprobability@gmail.com>
2020-02-16 16:06:48 -0800

    bump tensorflow to 1.15.2, bump semver to 1.0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "pillow>=6.2.0",
         "pyautogui",
         "pyscreenshot",
-        "tensorflow==1.15.0",
+        "tensorflow==1.15.2",
         "tesserocr",
     ],
     license="MIT license",
